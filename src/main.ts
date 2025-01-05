79978b37a9b66e0e51a5ccdaabc222b45e077742
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-05 10:09:16 +0530

    REad only property

--- src/main.ts
+++ src/main.ts
@@ -8,25 +8,22 @@ type User = {
   }
 }
 
-function createUserWithAddress(user: Required<User>) {
-  console.log(user)
-}
+type T = Readonly<User>
 
+function createUserWithAddress(user: Required<User>) {}
 function createUser(user: Omit<User, 'id'>) {}
 function updateUser(user: Partial<User>) {}
 function renderUserDetails(user: Pick<User, 'name' | 'age'>) {
   console.log(user.name, user.age)
 }
 
-// By using required its like all the keys in type are
-// we have pass otherwise it will give an error
+// When we declare the Readonly property for any type, that represents the immutabilty.
+// this same concept we use in js as object.freeze(user) ,, so user.id we cant change
+// same by declaring t as Readonly.
 
-createUserWithAddress({
-  name: 'casinor',
-  id: 'dfd',
-  age: 29,
-  address: {
-    street: 'sdfd',
-    city: 'fance'
-  }
-})
+// below we cannot change user.id as it represnet Readonly property on user, this is one level nesting
+// in address we can change the street and city
+
+let user: T
+
+user.id = 12 // this is readonly property
