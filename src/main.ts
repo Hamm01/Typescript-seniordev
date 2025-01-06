9d1653cb398c4a9ea49e506511582b0b83b7a48f
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-06 11:12:41 +0530

    Extract from the typescript used and explained

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
-type User = {
-  id: string
-  name: string
-  age: number
-  address: {
-    street: string
-    city: string
-  }
-}
+type Role = 'admin' | 'user' | 'moderator'
+type otherRole = 'testing' | 'admin' | 'user' | 'security'
 
-type T = Record<'admin' | 'user', { test: string }>
+type T = Extract<Role, otherRole>
 
-//Record is key value pair, we can user in key like admin and user, bothe with value as object taht take test as string
-const a: T = {
-  admin: { test: 'gamer' },
-  user: { test: 'dfd' }
-}
-console.log(a)
+// in this type T has both the common types like admin and user
+
+type B = Extract<Role, 'admin' | 'user'>
+
+// we can extract the type from role. only and admin and user will extracted and used in type B
