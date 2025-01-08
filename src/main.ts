4ab7c7a2c2b7c429d241d0411f82c2467560901f
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-08 08:51:13 +0530

    parameters function type , how to use

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,13 @@ type T = Parameters<typeof getUser>
 const a: T = ['coffers', 56]
 
 // in this the const variable a has type T that as tuple we need to pass a string and number.
+
+// where we need this paramets as type like two function below shown , if we change the parameter type form string to number in
+// below function getUser , we need to change mannualy type in wrapper function in id, so thats where we can user parmeters type
+function getUser(id: number) {
+  return { id, age: '12' }
+}
+
+function wrappergetUser(id: Parameters<typeof getUser>[0], message: string) {
+  return getUser(id)
+}
