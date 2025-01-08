95dae947e6ed49c8a551488c0eba9fd23c4176c0
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-08 08:45:45 +0530

    parameters for the function as type

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ function getUser(id: string, age: number) {
 
 type T = Parameters<typeof getUser>
 
-// this will pass the paramaters as tuples in the code
+// this will pass the paramaters as tuples in the code , means parameters of function will returned as array
 
-const a: T = ['fhsdfg', 44]
+const a: T = ['coffers', 56]
 
 // in this the const variable a has type T that as tuple we need to pass a string and number.
