4145f6a359e7486ff329e11609d84980e9b3c368
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-10 12:27:47 +0530

    Awaited in type script, that will return the promise return type

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-type A = string | null | undefined
+function getUser(id: string) {
+  return Promise.resolve({ name: 'selina' })
+}
 
-type T = NonNullable<A>
+type T = ReturnType<typeof getUser>
 
-// Type T is used which can be anything not null or undefined
+// This T type will return a promise version of getUser with type T = Promise<{ name: string;}>
+
+type B = Awaited<ReturnType<typeof getUser>>
+
+// this type B will not retuen the promise version but what promise will return after type B = { name: string;}
